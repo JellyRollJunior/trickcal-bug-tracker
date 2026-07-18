1426abc001ab412f7cc6c2a586ae7b5f38b4e3a4
Brandon Lin <brandonlin9716@gmail.com>
2026-07-18 11:20:31 -0700

    Configure prettier settings

--- apps/client/src/app/App.tsx
+++ apps/client/src/app/App.tsx
@@ -1,9 +1,9 @@
 function App() {
   return (
     <>
-     <h1 className="text-3xl">Supiki deruji bazeyo!</h1>
+      <h1 className="text-3xl">Supiki deruji bazeyo!</h1>
     </>
-  )
+  );
 }
 
-export default App
+export { App };
