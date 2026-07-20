9f1337f4f4b68121ee429dc506eabc4101c558b7
Brandon Lin <brandonlin9716@gmail.com>
2026-07-19 18:29:16 -0700

    Add user roles. POST /auth/signup creates a new user

--- apps/server/src/features/auth/controller.ts
+++ apps/server/src/features/auth/controller.ts
@@ -1,11 +1,26 @@
+import type { User } from '@prisma/client';
 import type { Request, Response, NextFunction } from 'express';
+import * as userQueries from '@/features/users/queries.js';
+import bcrypt from 'bcryptjs';
 
 const postSignup = async (
-    _req: Request<{}, {}, {}>,
+    req: Request<{}, {}, Pick<User, 'username' | 'password' | 'name'>>,
     res: Response,
-    _next: NextFunction
+    next: NextFunction
 ) => {
-    res.json("you signed up!")
+    try {
+        const { username, password, name } = req.body;
+        const hashedPassword = await bcrypt.hash(password ?? '', 10);
+        const newUser = await userQueries.createUser({
+            username,
+            password: hashedPassword,
+            name,
+        });
+
+        res.json(newUser);
+    } catch (error) {
+        next(error);
+    }
 };
 
 export { postSignup };
