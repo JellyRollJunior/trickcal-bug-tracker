98c3f731a830237b808c2dd03fa375579fd2da35
Brandon Lin <brandonlin9716@gmail.com>
2026-07-21 22:32:06 -0700

    Implement create board query to create board with default sections

--- apps/server/src/features/boards/board.queries.ts
+++ apps/server/src/features/boards/board.queries.ts
@@ -1,5 +1,5 @@
 import { prisma } from '@/db/prisma.js';
-import { User } from '@prisma/client';
+import { Board, User } from '@prisma/client';
 import { DatabaseError } from '@trickcal-bug-tracker/shared';
 
 const getBoards = async ({ id: userId }: Pick<User, 'id'>) => {
@@ -19,4 +19,33 @@ const getBoards = async ({ id: userId }: Pick<User, 'id'>) => {
     }
 };
 
-export { getBoards }
+/* Create board with default sections */
+const createBoard = async ({
+    id: userId,
+    name,
+    description,
+}: Pick<User, 'id'> & Pick<Board, 'name' | 'description'>) => {
+    try {
+        const data = await prisma.board.create({
+            data: {
+                ownerId: userId,
+                name: name,
+                description: description,
+                sections: {
+                    create: [
+                        { order: '1', name: 'Backlog' },
+                        { order: '2', name: 'In Development' },
+                        { order: '3', name: 'Testing' },
+                        { order: '4', name: 'Blocked' },
+                        { order: '5', name: 'Done' },
+                    ],
+                },
+            },
+        });
+        return data;
+    } catch (error) {
+        throw new DatabaseError('Unable to create board');
+    }
+};
+
+export { getBoards, createBoard };
